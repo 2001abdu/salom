6a7479d954d65554a81b2dd862d985fc58539d56
xudoyberdiyevabdulaziz <abdulazizxudoyberdiyev555@gmail.com>
2023-02-06 18:51:35 +0500

    salom javascript

---
+++ index.js
@@ -0,0 +1,386 @@
+// integer 11-20 uyga vazifa
+
+// let a=2;
+// let b=3
+// console.log(a+b + " raqamlar yig'indisi");
+// let a=5;
+// let b=6;
+// console.log(a*b + " raqamlar ko'paytmasi");
+
+// 2 uyga vazifa
+// let n = Number(prompt("n sonni kiriting"));
+// console.log(Math.pow(n+3,2));
+
+// 3 uyga vazifa
+// let n = Number(prompt("n ning qiymaini kiriting"));
+// console.log(Math.pow((3+n)/2,2));
+
+// 4 uyga vazifa
+// let n = Number(prompt("n ning qiymatini kiriting"))
+// let x=2;
+// console.log(Math.pow((n**x)+(6**x),2));
+
+// 5 uyga vazifa
+// let a= prompt("a ning ixtiyoriy qiymatini kiriting");
+// console.log(Math.abs(a));
+
+// 6 uyga vazifa
+// let m = prompt("m sonni kiriting");
+// m++;
+// console.log(Math.round(m));
+
+// // 7 uyga vazifa
+// let x = prompt("x ning qiymatini kiriting");
+// let y = prompt("y ning qiymatini kiriting");
+
+// console.log(Math.pow(x**2,2) + Math.pow(5*x,2) + Math.pow(x,2)*x*y);
+
+// 8 uyga vazifa
+// let x = prompt("x ning qiymatini kiriting");
+// let y = prompt("y ning qiymatini kiriting");
+
+// console.log(x*y**3*(Math.pow(x*y,2)*6 + Math.pow(x**2,2)*4) - 24*x*y);
+
+// 9 uyga vazifa
+// let a= 230;
+// console.log( 2 + 3 + 0 + " raqamlari yig'indisi " + " , ", 2*3*0 + " raqamlar ko'paytmasi ");
+
+// 10 uyga vazifa
+// console.log(Math.abs((5/3) - (13/4)));
+
+
+
+
+// integer bo'yicha uyga vazifalar 
+
+// integer 1 uyga vazifa
+
+// let a=prompt(" a ning qiymatini kiriting");
+// let b=prompt(" b ning qiymatini kiriting");
+// console.log(Math.sqrt( Math.sqrt((a**3 - b**3) + (a**3 + b**3))));
+
+// integer 2 uyga vazifa
+
+// let a=prompt("a ning qiymatini kiriting");
+// let b=prompt("b ning qiymatini kiriting");
+// console.log(Math.pow(1/6*Math.sqrt(a) + 1/3*Math.sqrt(b),2));
+
+// integer 3 uyga vazifa
+
+// let a=231;
+// let fx = (a%2);
+// let fy = (a%2)+2;
+// let fz= (a%2)+1;
+// console.log(fx*100 +fy*10 + fz);
+
+
+// Integer 4 uyga vazifa
+
+// let a=345;
+// let fx=(a%2)+4
+// let fy=(a%2)+3
+// let fz=(a%2)+2
+// console.log(fx*100+fy*10+fz);
+
+// integer 5 uyga vazifa
+
+// let a=prompt("a ning qiymatini kiriting");
+// let b=prompt("b ning qiymatini kiriting");
+
+// console.log(Math.round(9*a*b*(a-3*a*b) + 15*b**2));
+
+// integer 6 uyga vazifa
+// let r=prompt("r ning qiymatini kiriting");
+// let a=prompt("a ning qiymatini kiriting");
+// let b=prompt("b ning qiymatini kiriting");
+// let n=prompt("n ning qiymatini kiriting");
+
+// console.log((1 + r/100)**n / Math.sqrt(a**2 + b**2));
+
+
+// integer 7 uyga vazifa
+// let x1= prompt("x1 ning qiymatini kiriting");
+// let y1= prompt("y1 ning qiymatini kiriting");
+// let x2= prompt("x2 ning qiymatini kiriting");
+// let y2= prompt("y2 ning qiymatini kiriting");
+
+// console.log(Math.sqrt((x2-x1)**2 + (y2-y1)**2));
+
+// integer 8 uyga vazifa
+
+// let a=prompt("a ning qiymatini kiriting");
+// let b=prompt("a ning qiymatini kiriting");
+// let c=prompt("a ning qiymatini kiriting");
+// let p=(a+b+c)/2;
+// console.log(p);
+// console.log(Math.round(Math.sqrt(p*(p-a)*(p-b)*(p-c))));
+
+// integer 9 uyga vazifa
+
+// let a=prompt("a ning qiymatini kiriting");
+// console.log(Math.sin(a));
+
+// integer 10 uyga vazifa
+// let a=prompt("a ning qiymatini kiriting");
+// console.log(Math.cos(a));
+
+
+
+// if else uyga vazifa
+
+// if 1 uyga vazifa
+
+// let a=5;
+// let b=8;
+// let c=1;
+// if(a<b<c){
+//     console.log(a);
+// }else if(a>c>b){
+//     console.log(b);
+// }else if(a>b>c){
+//     console.log(c);
+// }else if(a<b || b>c){
+//     console.log(c);
+// }
+
+
+// if 2 uyga vazifa
+// let a = 50;
+// let b = 89;
+// let c = 1;
+
+// if(a>b || b>a ){
+//     console.log(a);
+// }else if(a>c || c>a){
+//     console.log(c);
+// }else if(a>b>c || a<b<c){
+//     console.log(b);
+// }
+
+
+// if 3 uyga vazifa
+
+// let a=prompt("a ning qiymatini kiriting");
+// let b=prompt("a ning qiymatini kiriting");
+// let z=0;
+// if(a>b){
+//     console.log(a);
+// }else if(a<b){
+//     console.log(b);
+// }else if(a==b){
+//     console.log(z);
+// }
+
+// if 4 uyga vazifa
+
+// let a=prompt("a ning qiymatini kiriting");
+// let b=prompt("b ning qiymatini kiriting");
+// if (a>0 || b>0) {
+//     console.log("1 chorakda yoki 3 chorakda");
+// }else if(a<0 || b<0){
+//     console.log("2 chorakda yoki 4 chorakda");
+// }
+
+
+// if 5 uyga vazifa
+
+// let a=prompt("a ning qiymatini kiriting");
+
+// let fx;
+// if (a>0) {
+//     console.log(fx=2*Math.sin(a));
+// }else if(a <= 0){
+//     console.log(fx =(a-6));
+// }else{
+//     console.log(a);
+// }
+
+
+// if 6 uyga vazifa
+
+// let x=prompt("x ning qiymatini kiriting");
+// let fx;
+
+// if(x < -2 || x > 2){
+//     console.log(fx = 2*x);
+// }else{
+//     console.log(fx= -3*x);
+// }
+
+// if 7 uyga vazifa
+
+// let x=prompt("x ning qiymatini kiriting");
+
+// let fx;
+// if(x <= 0){
+//     console.log(fx=-x);
+// }else if(0<x<2){
+//     console.log(fx=x**2);
+// }else if(x>=2){
+//     console.log(fx=4);
+// }else{
+//     console.log(fx=x);
+// }
+
+// if 8 uyga vazifa
+
+// let a= prompt("a ning qiymatini kiriting");
+// let b= prompt("b ning qiymatini kiriting");
+// let c= prompt("c ning qiymatini kiriting");
+
+// if(a+b>c){
+//     console.log("yes");
+// }else if(a+c>b){
+//     console.log("yes");
+// }else if(b+c>a){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// }
+
+// if 9 uyga vazifa
+// let a = prompt("a ning qiymatini kiriting");
+
+// if(a<0){
+//     console.log("Freezing");
+// }else if(a<10){
+//     console.log("Very Cold");
+// }else if(a<20){
+//     console.log("Cold");
+// }else if(a<30){
+//     console.log("Normal");
+// }else if(a<40){
+//     console.log("Hot");
+// }else if(a>40){
+//     console.log("Very Hot");
+// }else{
+//     console.log("bunday qiymat mavjud emas");
+// }
+
+
+// if 10 uyga vazifa
+
+// let a = prompt("a ning qiymatini kiriting");
+
+// if(a%2==0){
+//     console.log("musbat juft son");
+// }else if(a%2==1){
+//     console.log("musbat toq son");
+// }else if(a%2==-1){
+//     console.log("manfiy toq son");
+// }else if(a%2<=-1){
+//     console.log("musbat juft son");
+// }else{
+//     console.log("nolga teng");
+// }
+
+// if 11 uyga vazifa ko'rib chiaqaman hozir
+
+// let a = 93;
+// if(a / 100>=1 && a%2==0){
+//     console.log("3 xonali juft son");
+// }else if(a / 100>=1 && a % 2==1){
+//     console.log("3 xonali toq son");
+// }else if(a / 10>=1 && a%2==0){
+//     console.log("2 xonali juft son");
+// }else if(a / 10>=1 && a%2==1){
+//     console.log("2 xonali toq son");
+// }
+
+// if(!(a%2 == 0) || a<100){
+//     console.log("ikki xonali juft son");
+// }else if(a%2 == 1 || a<100){
+//     console.log("ikki xonali toq son");
+// }else{
+//     console.log("bunday qiymat mavjud emas");
+// }
+
+// if 12 uyga vazifa
+// let n=prompt("ning qiymatini kirit");
+// console.log(Math.pow(n*1,2));
+
+// if 13 uyga vazifa
+// let summ=20;
+// let count=true;
+// if(summ <= 20 && true){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+// if 14 uyga vazifa
+
+// let n =27;
+// let fx= (n%2)+6;
+// let fy= (n%2)+1
+
+// console.log(fx*10+fy>=n);
+
+// 15 uyga vazifa
+
+// let a=8481;
+
+// if(a/1000>=1){
+//     console.log("4 xonali son");
+// }else if(a/100>=1){
+//     console.log("3 xonali son");
+// }else if(a/10>=1){
+//     console.log("2 xonali son");
+// }
+
+// 16 uyga vazifa
+
+// let a=45;
+// let fx=a%2+4;
+// let fy=a%2+3;
+// console.log(fx+fy);
+
+// let b=345;
+// let fz=b%2+4;
+// let fk=b%2+3;
+// let fa=b%2+2;
+// console.log(fa+fk+fz);
+
+// 17 uyga vazifa
+
+// let a=45;
+// let fx=a%2+4;
+// let fz=a%2+2;
+
+// if (fx%2==1) {
+//     console.log(fx);
+// }else if(fz%2==1){
+//     console.log(fz);
+// }else if(fx%2==0){
+//     console.log("juft son");
+// }else if(fz%2==0){
+//     console.log("juft son");
+// }
+
+
+
+// if 18 uyga vazifa
+
+// let a=prompt("a ning qiymatini kiriting");
+// let b=prompt("b ning qiymatini kiriting");
+// let c=prompt("c ning qiymatini kiriting");
+// if(a>b>c && a>b && a>c){
+//     console.log(a);
+// }else if(b>a>c && a<b && c<b){
+//     console.log(b);
+// }else if(a<b<c && a<c && b<c){
+//     console.log(c);
+// }else if(a>b && b<c && a>c){
+//     console.log(a);
+
+// if 19 uyga vazifa
+
+// let a= 56987;
+// let fx=a%2+6;
+// let fy=a%2+7;
+// let fz=a%2+8;
+// let fa=a%2+5;
+// let fk=a%2+4;
+
+
